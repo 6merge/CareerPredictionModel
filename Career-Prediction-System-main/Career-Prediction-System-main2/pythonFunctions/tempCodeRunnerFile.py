2478c61abc90936809a3aa1b19408d716b7339d2
6merge <142732692+6merge@users.noreply.github.com>
2024-03-03 23:06:42 +0530

    Add files via upload

---
+++ Career-Prediction-System-main/Career-Prediction-System-main2/pythonFunctions/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+# # Decision Tree Model
+# file1 = open('pkl/model1.pkl', 'wb') 
+# pickle.dump(clf1, file1)
+# file1.close()
+
+# # SVM Model
+# file2 = open('pkl/model2.pkl', 'wb') 
+# pickle.dump(clf2, file2)
+# file2.close()
